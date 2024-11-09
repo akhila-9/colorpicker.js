3a77698e0a414e4a36f11b1d273c589f48ca05b1
akhilagit09 <120798830+akhilagit09@users.noreply.github.com>
2024-11-09 20:25:07 +0530

    intial commit

---
+++ js1.js
@@ -0,0 +1,26 @@
+let colorPicker=document.getElementById("colorPickerContainer");
+let colorcode=document.getElementById("selectHexCode");
+console.log("colorPicker");
+console.log("colorcode");
+
+function btn1(){
+    colorPicker.style.backgroundColor="#e0e0e0";
+    colorcode.textContent="#e0e0e0";
+    colorcode.style.color="#49a6e9";
+}
+function btn2(){
+    colorPicker.style.backgroundColor="#6fcf97";
+    colorcode.textContent="#6fcf97";
+    colorcode.style.color="#49a6e9";
+}
+function btn3(){
+    colorPicker.style.backgroundColor="#56ccf2";
+    colorcode.textContent="#56ccf2";
+    colorcode.style.color="#49a6e9";
+}
+function btn4(){
+    colorPicker.style.backgroundColor="#bb6bd9";
+    colorcode.textContent="#bb6bd9";
+    colorcode.style.color="#49a6e9";
+}
+
